7b5fad00b3226a226113c2fa5d03e7c5c0fc12c1
Sovit Bhandari <bhandari4@usf.edu>
2025-05-30 10:43:51 -0400

    Moved train.csv to data-files folder

--- First-ML-Model.py
+++ First-ML-Model.py
@@ -1,24 +1,24 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 
-data_path = 'train.csv'
+data_path = 'data-files/train.csv'
 read_data = pd.read_csv(data_path)
 # print(read_data.columns)
 
-'''Specify Prediction Target'''
+#Specify Prediction Target
 y = read_data.SalePrice
 
 feature_names = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 X = read_data[feature_names]
 
 
-'''model reproducibility'''
+# model reproducibility
 decision_model = DecisionTreeRegressor(random_state = 1)
 
-'''Fit the model'''
+# Fit the model
 decision_model.fit(X,y)
 
-'''Model's prediction'''
+#Model's prediction
 print("Making Predictions for first 5 houses:")
 print(X.head())
 print("The predicted prices are")
